7468bfea81d85c03c920cd5b54bb7986de73f94d
Denis Churbanov <4urbanoff@gmail.com>
2022-03-13 10:52:50 +0300

    pass __variables to style attribute

--- src/factory.ts
+++ src/factory.ts
@@ -6,20 +6,44 @@ type Styles = { [k: string]: string }
 export const createComponent = (tag: Tag, styles: Styles) => {
     return (props: JSX.IntrinsicAttributes & { [k in keyof Styles]: boolean }) => {
         const attributes = {} as { [k: string]: any }
+        const style = {} as { [k: string]: any }
         const classNames = []
 
         for (const [key, val] of Object.entries(props)) {
+            if (key.startsWith('__')) {
+                const property = '--' + key.slice(2)
+
+                style[property] = val
+
+                continue
+            }
+
+            if (key === 'style') {
+                if (typeof val === 'object') {
+                    Object.assign(style, val)
+                }
+                continue
+            }
+
             if (key === 'className') {
                 if (!!val) {
                     classNames.push(val)
                 }
-            } else if (key in styles) {
+                continue
+            }
+
+            if (key in styles) {
                 if (val) {
                     classNames.push(styles[key])
                 }
-            } else {
-                attributes[key as string] = val
+                continue
             }
+
+            attributes[key as string] = val
+        }
+
+        if (!!Object.keys(style).length) {
+            attributes.style = style
         }
 
         if (!!classNames.length) {
